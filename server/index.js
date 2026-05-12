b3d0be0ad37e2068efb61524f8948c8b4105cc63
Andrés Abad <andresabadpm@gmail.com>
2026-05-11 21:29:29 -0500

    binded server to 0.0.0.0 no localhost

--- server/index.js
+++ server/index.js
@@ -879,6 +879,6 @@ app.post('/api/friends/presence', w(async (req, res) => {
   res.json({ ok: true });
 }));
 
-server.listen(PORT, () => {
-  console.log(`\n📚  Cursos Studio corriendo en http://localhost:${PORT}\n`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
